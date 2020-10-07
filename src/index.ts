b729e0617dbf1ef521dac35e499d0b3282bd6eed
Nastya07s <nastya.kotovich07@gmail.com>
2020-10-07 20:48:41 +0300

    feat: add redirect http to https

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,18 @@ import express = require('express');
 import dotenv = require('dotenv');
 import bodyParser = require('body-parser');
 
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
 // import { UserModel } from './models';
-import { authRoutes, commentRoutes, projectRoutes, taskRoutes, userRoutes } from './routes';
+import {
+  authRoutes,
+  commentRoutes,
+  projectRoutes,
+  taskRoutes,
+  userRoutes,
+} from './routes';
 const keys = require('./config/keys');
 const passport = require('passport');
 
@@ -31,14 +41,29 @@ app.use(function logMethodAndUrl(request, response, next) {
   next();
 });
 
+const portHttp = process.env.PORT_HTTP || 3001;
+const port = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if (!req.secure) {
+    return res.redirect(307, 'https://localhost:' + port + req.url);
+  }
+  next();
+});
+
 app.use('/api/project', projectRoutes);
 app.use('/api/task', taskRoutes);
 app.use('/api/comment', commentRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 
-const port = process.env.PORT || 3000;
+const options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert'),
+};
 
-app.listen(port, function () {
+http.createServer(app).listen(portHttp);
+
+https.createServer(options, app).listen(port, function () {
   console.log(`App is listening on port ${port}!`);
 });
